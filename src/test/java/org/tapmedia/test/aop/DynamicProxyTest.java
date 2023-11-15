658c3db5102e7ecd36f96f436457ba3cb0a5555c
Natrix <76524304+luongtrieuvy202@users.noreply.github.com>
2023-11-15 18:38:29 +0700

    add cglib proxy

--- src/test/java/org/tapmedia/test/aop/DynamicProxyTest.java
+++ src/test/java/org/tapmedia/test/aop/DynamicProxyTest.java
@@ -1,10 +1,12 @@
 package org.tapmedia.test.aop;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.tapmedia.aop.AdvisedSupport;
 import org.tapmedia.aop.MethodMatcher;
 import org.tapmedia.aop.TargetSource;
 import org.tapmedia.aop.aspectj.AspectjExpressionPointcut;
+import org.tapmedia.aop.framework.CglibAopProxy;
 import org.tapmedia.aop.framework.JdkDynamicAopProxy;
 import org.tapmedia.test.ioc.common.WorldServiceInterceptor;
 import org.tapmedia.test.ioc.service.WorldService;
@@ -12,11 +14,13 @@ import org.tapmedia.test.ioc.service.WorldServiceImpl;
 
 public class DynamicProxyTest {
 
-	@Test
-	public void testJdkDynamicProxy() throws Exception {
+	private AdvisedSupport advisedSupport;
+
+	@Before
+	public void setup() {
 		WorldService workService = new WorldServiceImpl();
 
-		AdvisedSupport advisedSupport = new AdvisedSupport();
+		this.advisedSupport = new AdvisedSupport();
 		TargetSource targetSource = new TargetSource(workService);
 		WorldServiceInterceptor methodInterceptor = new WorldServiceInterceptor();
 		MethodMatcher methodMatcher = new AspectjExpressionPointcut(
@@ -25,7 +29,17 @@ public class DynamicProxyTest {
 		advisedSupport.setTargetSource(targetSource);
 		advisedSupport.setMethodInterceptor(methodInterceptor);
 		advisedSupport.setMethodMatcher(methodMatcher);
+	}
+
+	@Test
+	public void testJdkDynamicProxy() throws Exception {
 		WorldService proxy = (WorldService) new JdkDynamicAopProxy(advisedSupport).getProxy();
+		proxy.explode();
+	}
+
+	@Test
+	public void testCglibDynamicProxy() throws Exception {
+		WorldService proxy = (WorldService) new CglibAopProxy(advisedSupport).getProxy();
 		proxy.explode();
 	}
 
